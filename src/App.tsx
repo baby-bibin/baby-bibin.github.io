99cbeb9967ae7ac5c19e8ee2d9a7dca9a8bffb93
Bibin Baby <bibinbaby1996@gmail.com>
2024-08-22 01:25:41 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Education from "./components/Education/Education";
 import Works from "./components/Works/Works";
 import Certifications from "./components/Certifications/Certifications";
 import { useEffect, useState } from "react";
-const base_url = process.env.API || "https://portfolio-be-u1u3.onrender.com";
 interface MasterData {
   profileCardDetails: any;
   summaryCardDetails: any;
@@ -16,6 +15,8 @@ interface MasterData {
   education: any;
   works: any;
 }
+
+const base_url = process.env.REACT_APP_API || "http://localhost:3000";
 
 function App() {
   const [masterData, setMasterData] = useState<MasterData>();
@@ -36,28 +37,30 @@ function App() {
   }, []);
 
   return (
-    masterData && (
-      <Container fluid>
-        <Row>
-          <Col>
-            <div className="mainContainer">
-              <Profile details={masterData.profileCardDetails} />
-              <Summary details={masterData.summaryCardDetails} />
-              <Row>
-                <Col sm={12} md={12} lg={6} xl={6} xxl={6}>
-                  <Experience details={masterData.experience} />
-                </Col>
-                <Col sm={12} md={12} lg={6} xl={6} xxl={6}>
-                  <Certifications details={masterData.certifications} />
-                  <Education details={masterData.education} />
-                </Col>
-              </Row>
-              <Works details={masterData.works} />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    )
+    <>
+      {masterData && (
+        <Container fluid>
+          <Row>
+            <Col>
+              <div className="mainContainer">
+                <Profile details={masterData.profileCardDetails} />
+                <Summary details={masterData.summaryCardDetails} />
+                <Row>
+                  <Col sm={12} md={12} lg={6} xl={6} xxl={6}>
+                    <Experience details={masterData.experience} />
+                  </Col>
+                  <Col sm={12} md={12} lg={6} xl={6} xxl={6}>
+                    <Certifications details={masterData.certifications} />
+                    <Education details={masterData.education} />
+                  </Col>
+                </Row>
+                <Works details={masterData.works} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </>
   );
 }
 
